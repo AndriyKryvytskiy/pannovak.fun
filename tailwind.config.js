c2a8eb09b3132be9050bc1c3525ce5cf4db09671
AndriyKryvytskiy <77006358+AndriyKryvytskiy@users.noreply.github.com>
2025-04-24 16:45:12 +0200

    Merge remote-tracking branch 'origin/main'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // очень важно!
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
