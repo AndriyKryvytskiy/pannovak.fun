124ab66ef50764866c65379595fd69c03151885a
AndriyKryvytskiy <77006358+AndriyKryvytskiy@users.noreply.github.com>
2025-04-26 01:27:11 +0200

    Merge remote-tracking branch 'origin/main'

--- pages/od-autoru.js
+++ pages/od-autoru.js
@@ -18,7 +18,7 @@ export default function OdAutoru() {
       <div className="w-full md:w-2/3 flex flex-col gap-4">
         <div className="flex items-center gap-4">
           <Image
-            src="https://chatgptaihub.com/openai-chatgpt-logo/" // Добавь логотип сюда
+            src="chatgpt-logo.webp" // Добавь логотип сюда
             alt="ChatGPT logo"
             width={48}
             height={48}
