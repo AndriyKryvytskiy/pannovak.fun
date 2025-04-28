f60cc5287331baa4926d1396221cc0bb5b2a56c7
AndriyKryvytskiy <77006358+AndriyKryvytskiy@users.noreply.github.com>
2025-04-28 17:19:45 +0200

    npm run dev

---
+++ pages/test.js
@@ -0,0 +1,15 @@
+import ReactMarkdown from 'react-markdown';
+
+export default function TestPage() {
+  return (
+    <main className="min-h-screen bg-white p-10">
+      <div className="prose prose-lg max-w-none">
+        <ReactMarkdown>
+          {`### Vážený pane Nováku,
+
+obracím se na Vás v záležitosti, která vyžaduje jistou míru osobní angažovanosti.`}
+        </ReactMarkdown>
+      </div>
+    </main>
+  );
+}
