b79f1b90f74f4a3584b2ba4cc73af104e8a05151
AndriyKryvytskiy <77006358+AndriyKryvytskiy@users.noreply.github.com>
2025-04-28 17:30:10 +0200

    npm run dev

--- pages/test.js
+++ pages/test.js
@@ -1,10 +1,13 @@
-import ReactMarkdown from 'react-markdown';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
 export default function TestPage() {
   return (
     <main className="min-h-screen bg-white p-10">
       <div className="prose prose-lg max-w-none">
-        <ReactMarkdown>
+        <ReactMarkdown
+          components={{}}
+          remarkPlugins={[]}
+        >
           {`### Vážený pane Nováku,
 
 obracím se na Vás v záležitosti, která vyžaduje jistou míru osobní angažovanosti.`}
